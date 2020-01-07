f42df22d3763207508aaad349894a597ec7dbbbe
wwei <429744781@qq.com>
2020-01-07 17:08:10 +0800

    商品模块增加redis

---
+++ gmall-pms/src/main/java/com/ww/gmall/pms/config/PmsRedisConfig.java
@@ -0,0 +1,26 @@
+package com.ww.gmall.pms.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.net.UnknownHostException;
+
+/**
+ * 自定义redis对象序列化器
+ */
+@Configuration
+public class PmsRedisConfig {
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
+            throws UnknownHostException {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
