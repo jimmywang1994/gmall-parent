444233ef5ce0399662d7ea4a6f5e04e55a29ca18
Jimmy <429744781@qq.com>
2020-01-07 21:44:05 +0800

    三级分类菜单缓存

---
+++ gmall-api/src/main/java/com/ww/gmall/constant/SystemCacheConstant.java
@@ -0,0 +1,9 @@
+package com.ww.gmall.constant;
+
+/**
+ * 系统中使用的常量
+ */
+public class SystemCacheConstant {
+    public static final String CATEGORY_CACHED_KEY = "sys_category";
+
+}
--- gmall-pms/src/main/java/com/ww/gmall/pms/service/impl/ProductCategoryServiceImpl.java
+++ gmall-pms/src/main/java/com/ww/gmall/pms/service/impl/ProductCategoryServiceImpl.java
@@ -1,12 +1,15 @@
 package com.ww.gmall.pms.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ww.gmall.constant.SystemCacheConstant;
 import com.ww.gmall.pms.entity.ProductCategory;
 import com.ww.gmall.pms.mapper.ProductCategoryMapper;
 import com.ww.gmall.pms.service.ProductCategoryService;
 import com.ww.gmall.pms.vo.PmsProductCategoryWithChildrenItem;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -19,15 +22,27 @@ import java.util.List;
  * @author wwei
  * @since 2019-12-31
  */
+@Slf4j
 @Component
 @Service
 public class ProductCategoryServiceImpl extends ServiceImpl<ProductCategoryMapper, ProductCategory> implements ProductCategoryService {
 
     @Autowired
     ProductCategoryMapper productCategoryMapper;
+    @Autowired
+    RedisTemplate<Object, Object> redisTemplate;
+
     @Override
     public List<PmsProductCategoryWithChildrenItem> listCatalogWithChildren(Integer i) {
-        List<PmsProductCategoryWithChildrenItem> item=productCategoryMapper.listCatalogWithChildren(i);
-        return item;
+        List<PmsProductCategoryWithChildrenItem> items;
+        Object cachedMenu =redisTemplate.opsForValue().get(SystemCacheConstant.CATEGORY_CACHED_KEY);
+        if(cachedMenu!=null) {
+            items=(List<PmsProductCategoryWithChildrenItem>)cachedMenu;
+            log.info("菜单数据命中缓存");
+        }else{
+            items = productCategoryMapper.listCatalogWithChildren(i);
+            redisTemplate.opsForValue().set(SystemCacheConstant.CATEGORY_CACHED_KEY, items);
+        }
+        return items;
     }
 }
